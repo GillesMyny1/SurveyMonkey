0a990d265bd35fc81b0ca8629b97007dfe1d27c8
Gilles Myny <gillesmyny@cmail.carleton.ca>
2023-03-01 09:41:17 -0500

    Initial commit

---
+++ src/main/java/com/group11/surveymonkey/SurveyMonkeyApplication.java
@@ -0,0 +1,13 @@
+package com.group11.surveymonkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyMonkeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurveyMonkeyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group11/surveymonkey/SurveyMonkeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group11.surveymonkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyMonkeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
