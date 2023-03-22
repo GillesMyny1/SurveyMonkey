c3196ab99db6473ed44d51b59b9865b440a7f304
joshua-gatto <joshuagatto@cmail.carleton.ca>
2023-03-22 10:40:17 -0400

    Added execution timing to logging

--- src/main/java/com/group11/surveymonkey/loggingController/surveyLoggingController.java
+++ src/main/java/com/group11/surveymonkey/loggingController/surveyLoggingController.java
@@ -1,11 +1,14 @@
 package com.group11.surveymonkey.loggingController;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -15,11 +18,20 @@ public class surveyLoggingController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Pointcut(value="execution(* com.group11.surveymonkey.*.*(..))")
-    public void pointCut(){};
+    public void applicationPointCut(){};
 
-    @Before("pointCut()")
-    private void surveyLog(JoinPoint joinPoint){
+    @Before("applicationPointCut()")
+    private void executionLog(JoinPoint joinPoint){
         String methodName = joinPoint.getSignature().getName();
         logger.info("Survey method " + methodName + " executed.");
+    }
+
+    @Around("applicationPointCut()")
+    private void timingLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.nanoTime();
+        Object result = joinPoint.proceed();
+        long endTime = System.nanoTime();
+        String methodName = joinPoint.getSignature().getName();
+        logger.info("Survey Method " + methodName + " executed in " + ((endTime - startTime) / 100000) + " ms");
     }
 }
